332867490fda5f597cd7253d8ecb7edfbd737aba
Erick Llerenas <dev.llerenas@gmail.com>
2019-08-28 16:11:27 -0500

    Ya sirve el convert to HTML

--- js/index.js
+++ js/index.js
@@ -1,16 +1,12 @@
 function convert() {
-  preventDefault();
   selectedOption();
-  getFile();
 }
 
 function preventDefault() {
-  if (document.getElementById("file").value != "") {
-    const form = document.getElementById("form");
-    form.addEventListener("click", function(e) {
-      e.preventDefault();
-    });
-  }
+  const form = document.getElementById("form");
+  form.addEventListener("click", function(e) {
+    e.preventDefault();
+  });
 }
 
 function selectedOption() {
@@ -24,16 +20,39 @@ function selectedOption() {
       console.log("XML");
       break;
     case 3:
-      console.log("HTML");
+      convertToHTML();
   }
 }
 
-function getFile() {
-  const input = document.getElementById("file").value;
-  input.addEventListener(
-    "change",
-    function() {
-      console.log("something");
-    }
-  );
+function convertToHTML() {
+  preventDefault();
+
+  $(document).ready(function() {
+    $.ajax({
+      url: "archivo.csv",
+      dataType: "text",
+      success: function(data) {
+        var employee_data = data.split(/\r?\n|\r/);
+        var table_data = '<table class="table table-bordered table-striped">';
+        for (var count = 0; count < employee_data.length; count++) {
+          var cell_data = employee_data[count].split(",");
+          table_data += "<tr>";
+          for (
+            var cell_count = 0;
+            cell_count < cell_data.length;
+            cell_count++
+          ) {
+            if (count === 0) {
+              table_data += "<th>" + cell_data[cell_count] + "</th>";
+            } else {
+              table_data += "<td>" + cell_data[cell_count] + "</td>";
+            }
+          }
+          table_data += "</tr>";
+        }
+        table_data += "</table>";
+        $("#employee_table").html(table_data);
+      }
+    });
+  });
 }
