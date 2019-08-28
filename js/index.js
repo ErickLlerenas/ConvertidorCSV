3f9e8725ddc3d98284fadb241b2e4a442be26dc5
Erick Llerenas <dev.llerenas@gmail.com>
2019-08-28 12:54:55 -0500

    Segunda parte

--- js/index.js
+++ js/index.js
@@ -1,7 +1,39 @@
-function convert(){
-    var select = document.getElementById("select").value;
-    console.log(select);
+function convert() {
+  preventDefault();
+  selectedOption();
+  getFile();
+}
 
-    var file = document.getElementById("file").value;
-    console.log(file)
+function preventDefault() {
+  if (document.getElementById("file").value != "") {
+    const form = document.getElementById("form");
+    form.addEventListener("click", function(e) {
+      e.preventDefault();
+    });
+  }
+}
+
+function selectedOption() {
+  const select = document.getElementById("select").value;
+  option = parseInt(select);
+  switch (option) {
+    case 1:
+      console.log("JSON");
+      break;
+    case 2:
+      console.log("XML");
+      break;
+    case 3:
+      console.log("HTML");
+  }
+}
+
+function getFile() {
+  const input = document.getElementById("file").value;
+  input.addEventListener(
+    "change",
+    function() {
+      console.log("something");
+    }
+  );
 }
