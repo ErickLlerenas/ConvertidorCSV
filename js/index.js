9b17bfc9ec755a9e5af8c420c3dab5c85564687d
Erick Llerenas <dev.llerenas@gmail.com>
2019-08-27 20:34:07 -0500

    Primeros archivos

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function convert(){
+    var select = document.getElementById("select").value;
+    console.log(select);
+
+    var file = document.getElementById("file").value;
+    console.log(file)
+}
